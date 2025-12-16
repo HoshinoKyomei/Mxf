d9fa4704694c9af3ea0d0ec576fcc8d7119cbaa9
soatori cbsjz <cbsjz@qq.com>
2025-12-16 11:47:11 +0800

    feat(ability): 添加基础能力集和pawn数据类
    - 新增 BaseAbilitySet 类用于管理游戏能力、效果和属性集
    - 实现能力授予和移除的相关逻辑
    - 添加 BasePawnData 数据资产类定义pawn基础配置
    - 支持通过数据资产配置角色的能力集和输入设置

---
+++ Source/MxfGame/Character/BasePawnData.cpp
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BasePawnData.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(BasePawnData)
+
+UBasePawnData::UBasePawnData(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PawnClass = nullptr;
+	InputConfig = nullptr;
+}
+
---
+++ Source/MxfGame/Character/BasePawnData.h
@@ -0,0 +1,47 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/DataAsset.h"
+
+#include "BasePawnData.generated.h"
+
+class APawn;
+class UBaseAbilitySet;
+class UBaseAbilityTagRelationshipMapping;
+class UBaseInputConfig;
+class UObject;
+
+/**
+ * UBasePawnData
+ *
+ *	Non-mutable data asset that contains properties used to define a pawn.
+ */
+UCLASS(BlueprintType, Const, Meta = (DisplayName = "Base Pawn Data", ShortTooltip = "Data asset used to define a Pawn."))
+class MXFGAME_API UBasePawnData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UBasePawnData(const FObjectInitializer& ObjectInitializer);
+
+public:
+
+	// Class to instantiate for this pawn (should usually derive from ABasePawn or ABaseCharacter).
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Base|Pawn")
+	TSubclassOf<APawn> PawnClass;
+
+	// Ability sets to grant to this pawn's ability system.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Base|Abilities")
+	TArray<TObjectPtr<UBaseAbilitySet>> AbilitySets;
+
+	// What mapping of ability tags to use for actions taking by this pawn
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Base|Abilities")
+	TObjectPtr<UBaseAbilityTagRelationshipMapping> TagRelationshipMapping;
+
+	// Input configuration used by player-controlled pawns to create input mappings and bind input actions.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Base|Input")
+	TObjectPtr<UBaseInputConfig> InputConfig;
+	
+};
