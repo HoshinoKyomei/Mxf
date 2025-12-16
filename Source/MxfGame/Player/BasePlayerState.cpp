e1f5b63714baf8ce56cfe3e4ce63825ee88e547a
soatori cbsjz <cbsjz@qq.com>
2025-12-16 15:52:22 +0800

    feat(player): 实现玩家状态统计标签堆栈功能
    - 添加 FGameplayTagStack 和 FGameplayTagStackContainer 结构体
    - 在 ABasePlayerState 中增加 StatTags 容器支持
    - 实现标签堆栈的添加、移除及查询接口
    - 支持通过网络复制同步标签堆栈变化
    - 提供蓝图可调用的标签堆栈操作方法
    - 增加 PawnData 设置与初始化逻辑
    - 扩展玩家状态组件生命周期管理方法

--- Source/MxfGame/Player/BasePlayerState.cpp
+++ Source/MxfGame/Player/BasePlayerState.cpp
@@ -1,14 +1,30 @@
-﻿// Copyright Soatori Games, Inc. All Rights Reserved.
-
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "BasePlayerState.h"
 
-#include "BasePlayerController.h"
-#include "AbilitySystem/BaseAbilitySystemComponent.h"
 #include "AbilitySystem/Attributes/BaseCombatSet.h"
 #include "AbilitySystem/Attributes/BaseHealthSet.h"
+#include "AbilitySystem/BaseAbilitySet.h"
+#include "AbilitySystem/BaseAbilitySystemComponent.h"
+#include "Character/BasePawnData.h"
+#include "Character/BasePawnExtensionComponent.h"
+#include "Components/GameFrameworkComponentManager.h"
+#include "Engine/World.h"
+// #include "GameFramework/GameplayMessageSubsystem.h"
+// #include "GameModes/BaseExperienceManagerComponent.h"
+//@TODO: Would like to isolate this a bit better to get the pawn data in here without this having to know about other stuff
+#include "GameModes/BaseGameMode.h"
+#include "BaseLogChannels.h"
+#include "BasePlayerController.h"
+// #include "Messages/BaseVerbMessage.h"
+#include "Net/UnrealNetwork.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(BasePlayerState)
+
+class AController;
+class APlayerState;
+
+const FName ABasePlayerState::NAME_BaseAbilityReady("BaseAbilitiesReady");
 
 ABasePlayerState::ABasePlayerState(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -25,6 +41,55 @@ ABasePlayerState::ABasePlayerState(const FObjectInitializer& ObjectInitializer)
 	SetNetUpdateFrequency(100.0f);
 }
 
+void ABasePlayerState::PreInitializeComponents()
+{
+	Super::PreInitializeComponents();
+}
+
+void ABasePlayerState::Reset()
+{
+	Super::Reset();
+}
+
+void ABasePlayerState::ClientInitialize(AController* C)
+{
+	Super::ClientInitialize(C);
+
+	if (UBasePawnExtensionComponent* PawnExtComp = UBasePawnExtensionComponent::FindPawnExtensionComponent(GetPawn()))
+	{
+		PawnExtComp->CheckDefaultInitialization();
+	}
+}
+
+void ABasePlayerState::CopyProperties(APlayerState* PlayerState)
+{
+	Super::CopyProperties(PlayerState);
+
+	//@TODO: Copy stats
+}
+
+void ABasePlayerState::OnDeactivated()
+{
+	Super::OnDeactivated();
+}
+
+void ABasePlayerState::OnReactivated()
+{
+	Super::OnReactivated();
+}
+
+void ABasePlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	FDoRepLifetimeParams SharedParams;
+	SharedParams.bIsPushBased = true;
+
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass, PawnData, SharedParams);
+
+	DOREPLIFETIME(ThisClass, StatTags);	
+}
+
 ABasePlayerController* ABasePlayerState::GetBasePlayerController() const
 {
 	return Cast<ABasePlayerController>(GetOwner());
@@ -38,7 +103,62 @@ UAbilitySystemComponent* ABasePlayerState::GetAbilitySystemComponent() const
 void ABasePlayerState::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
-	
+
 	check(AbilitySystemComponent);
 	AbilitySystemComponent->InitAbilityActorInfo(this, GetPawn());
+}
+
+void ABasePlayerState::SetPawnData(const UBasePawnData* InPawnData)
+{
+	check(InPawnData);
+
+	if (GetLocalRole() != ROLE_Authority)
+	{
+		return;
+	}
+
+	if (PawnData)
+	{
+		UE_LOG(LogBase, Error, TEXT("Trying to set PawnData [%s] on player state [%s] that already has valid PawnData [%s]."), *GetNameSafe(InPawnData), *GetNameSafe(this), *GetNameSafe(PawnData));
+		return;
+	}
+
+	MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, PawnData, this);
+	PawnData = InPawnData;
+
+	for (const UBaseAbilitySet* AbilitySet : PawnData->AbilitySets)
+	{
+		if (AbilitySet)
+		{
+			AbilitySet->GiveToAbilitySystem(AbilitySystemComponent, nullptr);
+		}
+	}
+
+	UGameFrameworkComponentManager::SendGameFrameworkComponentExtensionEvent(this, NAME_BaseAbilityReady);
+	
+	ForceNetUpdate();
+}
+
+void ABasePlayerState::OnRep_PawnData()
+{
+}
+
+void ABasePlayerState::AddStatTagStack(FGameplayTag Tag, int32 StackCount)
+{
+	StatTags.AddStack(Tag, StackCount);
+}
+
+void ABasePlayerState::RemoveStatTagStack(FGameplayTag Tag, int32 StackCount)
+{
+	StatTags.RemoveStack(Tag, StackCount);
+}
+
+int32 ABasePlayerState::GetStatTagStackCount(FGameplayTag Tag) const
+{
+	return StatTags.GetStackCount(Tag);
+}
+
+bool ABasePlayerState::HasStatTag(FGameplayTag Tag) const
+{
+	return StatTags.ContainsTag(Tag);
 }
--- Source/MxfGame/Player/BasePlayerState.h
+++ Source/MxfGame/Player/BasePlayerState.h
@@ -5,9 +5,11 @@
 #include "CoreMinimal.h"
 #include "AbilitySystemInterface.h"
 #include "ModularPlayerState.h"
+#include "System/GameplayTagStack.h"
 
 #include "BasePlayerState.generated.h"
 
+class UBasePawnData;
 class ABasePlayerController;
 class UAbilitySystemComponent;
 class UBaseAbilitySystemComponent;
@@ -33,9 +35,48 @@ public:
 	UBaseAbilitySystemComponent* GetBaseAbilitySystemComponent() const { return AbilitySystemComponent; }
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	
+	template <class T>
+	const T* GetPawnData() const { return Cast<T>(PawnData); }
+
+	void SetPawnData(const UBasePawnData* InPawnData);
+
 	//~AActor interface
+	virtual void PreInitializeComponents() override;
 	virtual void PostInitializeComponents() override;
 	//~End of AActor interface
+
+	//~APlayerState interface
+	virtual void Reset() override;
+	virtual void ClientInitialize(AController* C) override;
+	virtual void CopyProperties(APlayerState* PlayerState) override;
+	virtual void OnDeactivated() override;
+	virtual void OnReactivated() override;
+	//~End of APlayerState interface
+	
+	static const FName NAME_BaseAbilityReady;
+	
+	// Adds a specified number of stacks to the tag (does nothing if StackCount is below 1)
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category=Teams)
+	void AddStatTagStack(FGameplayTag Tag, int32 StackCount);
+
+	// Removes a specified number of stacks from the tag (does nothing if StackCount is below 1)
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category=Teams)
+	void RemoveStatTagStack(FGameplayTag Tag, int32 StackCount);
+
+	// Returns the stack count of the specified tag (or 0 if the tag is not present)
+	UFUNCTION(BlueprintCallable, Category=Teams)
+	int32 GetStatTagStackCount(FGameplayTag Tag) const;
+
+	// Returns true if there is at least one stack of the specified tag
+	UFUNCTION(BlueprintCallable, Category=Teams)
+	bool HasStatTag(FGameplayTag Tag) const;
+	
+protected:
+	UFUNCTION()
+	void OnRep_PawnData();
+
+	UPROPERTY(ReplicatedUsing = OnRep_PawnData)
+	TObjectPtr<const UBasePawnData> PawnData;
 	
 private:
 	
@@ -49,4 +90,7 @@ private:
 	// Combat attribute set used by this actor.
 	UPROPERTY()
 	TObjectPtr<const class UBaseCombatSet> CombatSet;
+	
+	UPROPERTY(Replicated)
+	FGameplayTagStackContainer StatTags;
 };
